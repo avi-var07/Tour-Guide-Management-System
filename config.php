27ee2e748fbd480290ffa59909412cd323da6b31
Aviral Varshney <aviralvarshney07@gmail.com>
2025-04-08 10:08:18 +0530

    configured connection to establish only once by aviral

--- config.php
+++ config.php
@@ -5,12 +5,18 @@ $password = "";
 $dbname = "travel"; 
 $port = 3306; 
 
-define('SMTP_USER', 'aviralvarshney07@gmail.com');
-define('SMTP_PASS', 'dzbj qcar iihw lbga');
+// Define SMTP constants only if not already defined
+if (!defined('SMTP_USER')) {
+    define('SMTP_USER', 'aviralvarshney07@gmail.com');
+}
 
+if (!defined('SMTP_PASS')) {
+    define('SMTP_PASS', 'dzbj qcar iihw lbga');
+}
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-
-if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
